3beaf0442d3c01007646295f3211d5d91636f0c4
Heeral Jain <hiramanijain89@gmail.com>
2022-11-06 01:31:02 -0700

    Initial Commit

---
+++ State Managment/State Managment/State_ManagmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  State_ManagmentApp.swift
+//  State Managment
+//
+//  Created by Heeral Jain on 11/6/22.
+//
+
+import SwiftUI
+
+@main
+struct State_ManagmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
