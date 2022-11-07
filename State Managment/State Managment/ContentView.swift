775bc28b05b35fde597af37f63781293a34ed8e4
Heeral Jain <hiramanijain89@gmail.com>
2022-11-06 16:30:33 -0800

    Create User

--- State Managment/State Managment/ContentView.swift
+++ State Managment/State Managment/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var firstName = ""
+    @State var LastName = ""
+    
+    @State var users = [String]()
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            VStack{
+                VStack{
+                    VStack{
+                        Group{
+                            TextField("First Name", text: $firstName).padding(15)
+                        }.background(Color.gray).clipShape(RoundedRectangle(cornerRadius: 7)).shadow(radius: 7)
+                        Group{
+                            TextField("Last Name", text: $LastName).padding(15)
+                        }.background(Color.gray).clipShape(RoundedRectangle(cornerRadius: 7)).shadow(radius: 7)
+                        Button(action: {
+                            self.users.append("\(firstName) \(LastName)")
+                            self.firstName = ""
+                            self.LastName = ""
+                        }) {
+                            Group{
+                                Text("Create User").foregroundColor(.white).padding(12)
+                            }.background((firstName.count + LastName.count > 0) ? Color.indigo : Color.gray).clipShape(RoundedRectangle(cornerRadius: 7)).shadow(radius: 7)
+                        }
+                        Label("Swipe left to delete from list", systemImage: "Trash")
+                    }.padding(15)
+                    
+                }.background(Color.blue)
+                List{
+                    ForEach(users, id: \.self){ users in
+                        Text(users)
+                    }.onDelete(perform: {IndexSet in users.remove(atOffsets: IndexSet)})
+                    //                List(users, id: \.self) { users in
+                    //                    Text(users)
+                    //                }.onDelete(perform: {IndexSet in users.remove(atOffsets: IndexSet)})
+                }.navigationBarTitle(Text("Form"))
+                    .navigationBarItems(leading: HStack{
+                        Text("First Name:")
+                        Text(firstName).foregroundColor(.green)
+                        Text("Last Name:")
+                        Text(LastName).foregroundColor(.green)
+                        
+                    })
+            }
         }
-        .padding()
     }
 }
 
